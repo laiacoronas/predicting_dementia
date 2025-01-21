c33a737bc78f2fe095cbd2df77c6d1a540a95c36
Laia Coronas Sala <lclaiacoronas@gmail.com>
2025-01-21 15:37:34 -0300

    update

--- clean_transcripts.py
+++ clean_transcripts.py
@@ -8,26 +8,20 @@ transcripts = data['transcript']
 
 
 def preprocess_transcript(text):
-    # Eliminar líneas del investigador (INV)
-    text = re.sub(r"INV:.*?▶", "", text)  # Remover cualquier línea que empiece con INV
     
-    # Eliminar etiqueta "PAR:"
-    text = re.sub(r"PAR: ", "", text)  # Quitar "PAR: " al inicio de las líneas
+    text = re.sub(r"INV:.*?▶", "", text)  
     
-    # Eliminar contenido entre corchetes []
+    text = re.sub(r"PAR: ", "", text)  
+    
     text = re.sub(r"\[.*?\]", "", text)
     
-    # Eliminar caracteres "raros" como â–¶, â€¡, y similares
-    text = re.sub(r"[^\w\s.,]", "", text)  # Conserva solo letras, números, espacios, puntos y comas
+    text = re.sub(r"[^\w\s.,]", "", text)  
     
-    # Quitar espacios extra
     text = re.sub(r"\s+", " ", text).strip()
     return text
 
-# Aplicar la función a todas las transcripciones
 data['clean_transcripts'] = transcripts.apply(preprocess_transcript)
 
-# Guardar el resultado en un nuevo CSV
 output_file = "transcripts_cleaned.csv"
 data.to_csv(output_file, index=False)
 
